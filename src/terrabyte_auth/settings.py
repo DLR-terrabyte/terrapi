1e717dbccb452ae780637de3f9a00a41a45eeb37
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2024-02-10 00:38:48 +0100

    Cleanup. Adding function to wrap a request

--- src/terrabyte_auth/settings.py
+++ src/terrabyte_auth/settings.py
@@ -1,17 +1,6 @@
 import os
 
 
-TERRABYTE_PUBLIC_API_URL = os.environ.get(
-    "TERRABYTE_PUBLIC_API_URL",
-    # "https://gateway.terrabyte.eox.at/public/stac",
-    "https://terrabyte-api.hub.eox.at/public/stac"
-)
-TERRABYTE_PRIVATE_API_URL = os.environ.get(
-    "TERRABYTE_PRIVATE_API_URL",
-    # "https://gateway.terrabyte.eox.at/private/stac",
-    "https://terrabyte-api.hub.eox.at/private/stac"
-)
-
 TERRABYTE_AUTH_URL = os.environ.get(
     "TERRABYTE_AUTH_URL",
     "https://auth.terrabyte.lrz.de/realms/terrabyte/",
