50d74684675fa5ce168bba4dd84811350be9eeef
Jonas Eberle <jonas.eberle@gmx.de>
2024-07-11 16:51:34 +0200

    Cleanup settings.py

--- src/terrabyte_auth/settings.py
+++ src/terrabyte_auth/settings.py
@@ -1,17 +1,12 @@
 import os
 
-
 TERRABYTE_PUBLIC_API_URL = os.environ.get(
     "TERRABYTE_PUBLIC_API_URL",
-    # "https://gateway.terrabyte.eox.at/public/stac",
-    #"https://terrabyte-api.hub.eox.at/public/stac"
     "https://stac.terrabyte.lrz.de/public/api"
-
 )
+
 TERRABYTE_PRIVATE_API_URL = os.environ.get(
     "TERRABYTE_PRIVATE_API_URL",
-    # "https://gateway.terrabyte.eox.at/private/stac",
-    #"https://terrabyte-api.hub.eox.at/private/stac"
     "https://stac.terrabyte.lrz.de/private/api"
 )
 
@@ -22,6 +17,5 @@ TERRABYTE_AUTH_URL = os.environ.get(
 
 TERRABYTE_CLIENT_ID = os.environ.get(
     "TERRABYTE_CLIENT_ID",
-   # "at.eox.hub.terrabyte-api",
     "de.lrz.terrabyte.stac"
 )
