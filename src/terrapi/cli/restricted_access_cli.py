004a8b1766120613b360cc3049cee1fef4a8717a
Julian Zeidler <julian.zeidler@dlr.de>
2024-11-04 17:28:27 +0100

    initial implementation testet against restricted_data branch of the backend

--- src/terrapi/cli/restricted_access_cli.py
+++ src/terrapi/cli/restricted_access_cli.py
@@ -8,14 +8,16 @@ from terminaltables import AsciiTable
 
 @click.group()
 @click.pass_context
-def restricted_data(ctx:dict):
+@click.Option("-l", "local", help="Development Option to talk to backend locally (port 8000) ", default=False,type=bool)
+def restricted_data(ctx:dict,local:bool):
     """ Self Register to restricted Datasets on DSS. 
         This tool allows you to get an overview of currently available restricted datasets, your current access status and their usage restrictions/requirements. 
         Request access to datasets by accepting the specific EULAs 
     """
 
     ctx.obj['privateAPIUrl'] = TERRABYTE_RESTRICTED_DATA_API_URL
-    ctx.obj['privateAPIUrl'] = "http://127.0.0.1:8000"
+    if local:
+        ctx.obj['privateAPIUrl'] = "http://127.0.0.1:8000"
     pass
 
 @restricted_data.command()
@@ -46,41 +48,103 @@ def list_available(ctx:dict):
         click.echo(table.table)
 
 
-@restricted_data.command()
-@click.argument('dataset')
-@click.pass_context
-def request_access(ctx:dict,dataset:str):
-    """ Interactively request access to specific dataset on terrabyte DSS by accepting its EULAs 
-        Dataset can be provided by its ID or name
-    """
-    pass
+def get_container_info(ctx:dict,dataset:str)->dict:
+    url = f"{ctx.obj['privateAPIUrl']}/request/container/{dataset}"
+    container=None
+    try:   
+       r=wrap_request(requests.sessions.Session(),url=url,client_id=ctx.obj['ClientId'],method="GET")
+       if  r.status_code ==200:
+           response=r.json()
+           return response.get("container",None)
+       match r.status_code:
+            case 404:
+               click.echo(f"Error requested container '{dataset}' does not exist. Exiting")
+               return container 
+            
+            case 403:
+               click.echo(f"Error reported by Backend: {r.json().get('detail')} Exiting")
+               return container
+       r.raise_for_status()
+       
+    
+    except Exception as e:
+         click.echo(f"{e}")
+       # click.echo(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
+    return container
+
 
 @restricted_data.command()
 @click.argument('dataset')
 @click.pass_context
-def container_info(ctx:dict,dataset:str):
+def request_access(ctx:dict,dataset:str)->None:
     """ Interactively request access to specific dataset on terrabyte DSS by accepting its EULAs 
-        Dataset can be specfied either by its ID or its name
+        Dataset can be provided by its ID or name
+        Some datasets might be restriced to DLR Employees
     """
-    url = f"{ctx.obj['privateAPIUrl']}/request/container/{dataset}"
-    response=None
-    container=None
-    try:   
-       r=wrap_request(requests.sessions.Session(),url=url,client_id=ctx.obj['ClientId'],method="GET")
-       r.raise_for_status()
-       response=r.json()
-    except Exception as e:
-         click.echo(f"{e}")
-       # click.echo(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
-    if response:
-        container=response.get("container")
+    accessurl = f"{ctx.obj['privateAPIUrl']}/request/access/{dataset}"
+    container=get_container_info(ctx,dataset)
+    if not container:
+        return
+    status=container.get("status","")
+    if status =="registered":
+        click.echo(f"You allready have access to the container {container["name"]} and its dss {container["id"]}. What more do you need ;-)")
+        return
+    if status =="not-allowed":
+        click.echo(f"Unfortunatly your Account is not eligable for the container {container["name"]}.")
+        return
+    if status =="available":
+        click.echo(f"You are requesting access to the DSS Container {container["name"]}.")
+        click.echo("Please read the following Description and the specfied Documents thoroughly")
+        click.echo(" ")
         click.echo(f"Container Name:       {container["name"]}")
-        click.echo(f"Container ID:         {container["id"]}")
-        click.echo(f"Container Documents:  {container["hrefs"]}")
+        click.echo(f"Container DSS ID:     {container["id"]}")
+        click.echo("Container Documents:")
         for href in container["hrefs"]:
-            click.echo(f"                      {href}")
+            click.echo(f"{href}")
             #click.launch(href)
-        click.echo(f"Container Description: \n {container["description"]}")
+        click.echo(f"\nContainer Description: \n\n {container["description"]}")
+        click.echo("####################################################\n") 
+        eulaAccept=click.confirm(f"Do you confirm that you have read the Licence Agreement of the container {container["name"]} and that you accept the stated terms and contitions?", default=False, show_default =False)
+        if not eulaAccept:
+            click.echo("As you have not accepted the Licence Agreement, we can not request access. Sorry")
+            return
+    
+    click.echo("")
+    click.echo(f"Eula accepted, Requesting access to container {container["name"]}")
+    success=None
+    try:   
+        r=wrap_request(requests.sessions.Session(),url=f"{accessurl}?eulaAccept={eulaAccept}",client_id=ctx.obj['ClientId'],method="POST")
+        if r.status_code>=200 and r.status_code<299:
+           response=r.json()
+           success=response.get("status",None)
+        else:
+           click.echo(f"Error we received Code {r.status_code} from the Backend reporting: {r.json().get('detail',r.json())} ")
+           
+    except Exception as e:
+         click.echo(f"Unhandled Exception {e}")
+    if success:
+        click.echo(f"Status of access request was: {success}")
+
+        
+
+
+
+@restricted_data.command()
+@click.argument('dataset')
+@click.pass_context
+def request_info(ctx:dict,dataset:str):
+    """ Get detailed Description of a dataset container
+        Dataset can be specfied either by its ID or its name
+    """ 
+    container=get_container_info(ctx,dataset)
+    if container:
+        click.echo(f"Container Name:       {container["name"]}")
+        click.echo(f"Container DSS ID:     {container["id"]}")
+        click.echo("Container Documents:")
+        for href in container["hrefs"]:
+            click.echo(f"{href}")
+            #click.launch(href)
+        click.echo(f"\nContainer Description: \n\n {container["description"]}")
         
 
 
